90853925bea39a674adeb9c0051de1b12acba36c
Adrian Cole <acole@twitter.com>
2015-08-10 23:05:58 -0700

    Wires up cassandra test rule

--- zipkin-cassandra/src/test/scala/com/twitter/zipkin/storage/cassandra/CassieSpanStoreSpec.scala
+++ zipkin-cassandra/src/test/scala/com/twitter/zipkin/storage/cassandra/CassieSpanStoreSpec.scala
@@ -1,39 +1,41 @@
 package com.twitter.zipkin.storage.cassandra
 
+import com.datastax.driver.core.Cluster
 import com.twitter.app.App
-import com.twitter.cassie.tests.util.FakeCassandra
 import com.twitter.zipkin.cassandra.CassieSpanStoreFactory
 import com.twitter.zipkin.storage.SpanStoreSpec
-import org.junit.{BeforeClass, Ignore, Test}
+import java.util.Collections
+import org.cassandraunit.CassandraCQLUnit
+import org.cassandraunit.dataset.CQLDataSet
+import org.junit.ClassRule
 
 object CassieSpanStoreSpec {
-
-  // Extending FakeCassandra makes lifecycle hooks start, stop visible.
-  object FakeServer extends FakeCassandra
+  // Avoid conflicts with thrift 0.5
+  System.setProperty("cassandra.start_rpc", "false")
 
   // Scala cannot generate fields with public visibility, so use a def instead.
-  @BeforeClass def beforeAll() = FakeServer.start()
+  @ClassRule def cassandra = new CassandraCQLUnit(new CQLDataSet() {
+    override def isKeyspaceDeletion = true
+
+    override def getKeyspaceName = "zipkin"
+
+    override def isKeyspaceCreation = true
+
+    override def getCQLStatements = Collections.emptyList()
+  })
 }
 
 class CassieSpanStoreSpec extends SpanStoreSpec {
 
-  import CassieSpanStoreSpec.FakeServer
+  object TestStore extends App with CassieSpanStoreFactory
 
-  // Lazy to prevent initializing before embedded cassandra starts.
-  lazy val store = {
-    object CassieStore extends App with CassieSpanStoreFactory
-    CassieStore.main(Array("-zipkin.store.cassie.dest", "127.0.0.1:" + FakeServer.port.get))
-    CassieStore.newCassandraStore()
-  }
+  // unused currently
+  TestStore.main(Array("-zipkin.store.cassandra.dest", "127.0.0.1:9142"))
+  override lazy val store = TestStore.newCassandraStore(
+    Cluster.builder().addContactPoint("127.0.0.1").withPort(9142).build()
+  )
 
   override def clear = {
-    FakeServer.reset()
+    // class rule clears the keyspace
   }
-
-  /** Duration is unreliable in Cassie. */
-  @Ignore
-  @Test override def getTracesDuration() = super.getTracesDuration()
-
-  @Ignore
-  @Test override def getTraceIdsByName() = super.getTraceIdsByName()
 }
